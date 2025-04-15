700367d915e224db64e1134811bcdd05a2b0219e
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2025-04-15 12:29:54 +0200

    Created maven project

---
+++ src/main/java/pcd/ass02/App.java
@@ -0,0 +1,13 @@
+package pcd.ass02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
