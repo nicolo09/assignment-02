7679281a58a30e464e4e648a59629c0a1609e097
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2025-04-17 11:01:14 +0200

    Added basic tests

--- src/test/java/pcd/ass02/AppTest.java
+++ src/test/java/pcd/ass02/AppTest.java
@@ -1,6 +1,10 @@
 package pcd.ass02;
 
 import org.junit.jupiter.api.Test;
+import pcd.ass02.library.DependencyAnalyzer;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Unit test for simple App.
@@ -9,6 +13,35 @@ public class AppTest {
 
     @Test
     public void classDependenciesTest() {
+        new DependencyAnalyzer().getClassDependencies("src/main/java/pcd/ass02/library/DependencyAnalyzer.java")
+                .onComplete(result -> {
+                    if (result.succeeded()) {
+                        assertFalse(result.result().dependencies().isEmpty());
+                    } else {
+                        System.err.println("Failed to get dependencies: " + result.cause().getMessage());
+                    }
+                    System.exit(0);
+                });
         
+    }
+
+    @Test
+    public void packageDependenciesTest(){
+        /*
+        new DependencyAnalyzer().getPackageDependencies("src/main/java/pcd/ass02/library").onComplete(result->{
+            //todo: implement get package dependencies
+        });
+        */
+        assertThrows(UnsupportedOperationException.class, ()->new DependencyAnalyzer().getPackageDependencies("src/main/java/pcd/ass02/library"));
+    }
+
+    @Test
+    public void projectDependenciesTest(){
+        /*
+        new DependencyAnalyzer().getProjectDependencies("src/main/java/pcd/ass02/library").onComplete(result->{
+            //todo: implement get package dependencies
+        });
+        */
+        assertThrows(UnsupportedOperationException.class, ()->new DependencyAnalyzer().getProjectDependencies("src/main/java/pcd"));
     }
 }
